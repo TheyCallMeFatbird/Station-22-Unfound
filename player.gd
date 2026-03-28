e980fc4f69e358a236e2ca4f6eb0381b18b660ac
TheyCallMeFatbird <FatbirdYT@outlook.com>
2026-03-28 11:21:59 -0500

    fixed mouse & flashlight inputs! (attached diff script)

--- player.gd
+++ player.gd
@@ -1,17 +1,17 @@
 extends CharacterBody3D
 
 @export var speed = 5.0
-@export var mouse_sensitivity = 0.002
-@onready var flashlight = $Flashlight
-var rotation_x = 0.0
-@onready var flashlight_sound = $FlashlightSound
+@export var mouse_sensitivity = 0.004
 
+@onready var flashlight = $Flashlight
+@onready var flashlight_sound = $FlashlightSound
 @onready var camera = $Camera3D
 
+var rotation_x = 0.0
+
 func _ready():
-	if speed == null:
-		speed = 5.0
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	flicker_loop()
 
 func _input(event):
 	if event is InputEventMouseMotion:
@@ -19,13 +19,16 @@ func _input(event):
 		rotation_x -= event.relative.y * mouse_sensitivity
 		rotation_x = clamp(rotation_x, deg_to_rad(-80), deg_to_rad(80))
 		camera.rotation.x = rotation_x
+
+	if event is InputEventMouseButton:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
 	if event.is_action_pressed("flashlight_toggle"):
 		flashlight.visible = !flashlight.visible
 		flashlight_sound.play()
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	var direction = Vector3.ZERO
-
 	if Input.is_action_pressed("move_forward"):
 		direction -= transform.basis.z
 	if Input.is_action_pressed("move_backward"):
@@ -34,21 +37,16 @@ func _physics_process(delta):
 		direction -= transform.basis.x
 	if Input.is_action_pressed("move_right"):
 		direction += transform.basis.x
-
 	direction = direction.normalized()
 	velocity.x = direction.x * speed
 	velocity.z = direction.z * speed
-
 	move_and_slide()
 
 func flicker_loop():
 	while true:
 		await get_tree().create_timer(randf_range(3.0, 8.0)).timeout
-		
-		# tiny rapid flicker burst
 		for i in range(randi_range(1, 3)):
 			flashlight.light_negative = true
 			await get_tree().create_timer(0.03).timeout
-			
 			flashlight.light_negative = false
 			await get_tree().create_timer(0.03).timeout
